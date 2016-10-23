7e4cc84502a4992be5c1b68ee1e5493d2aa5561f
krzysztof <listdlakrzycha@gmail.com>
2016-10-23 15:53:40 +0200

    mime cutter

--- common/models/owner.js
+++ common/models/owner.js
@@ -1,5 +1,11 @@
 'use strict';
 
-module.exports = function(Owner) {
+module.exports = function (Owner) {
 
+
+  Owner.observe('after save', function (ctx, next) {
+
+    console.log("Komunikat po zapisie.", ctx.instance.ownerName);
+    next();
+  });
 };
