3a7b28a273e08019c2a986e28a520938479f62a5
krzysztof <listdlakrzycha@gmail.com>
2016-10-27 22:42:08 +0200

    mime cutter

--- common/models/owner.js
+++ common/models/owner.js
@@ -5,7 +5,7 @@ module.exports = function (Owner) {
 
   Owner.observe('after save', function (ctx, next) {
 
-    console.log("Komunikat po zapisie.", ctx.instance.ownerName);
+    console.log("Komunikat po zapisie. Obserwator.", ctx.instance.ownerName);
     next();
   });
 };
